c8ff540a4f84e4c7e16c9a4a7ffef0b8d95a5a21
gurramharish <gurramharish12@gmail.com>
2026-05-16 06:46:19 +0530

    consume using consumerrecord

--- src/main/java/com/hans/consumer/OrderConsumer.java
+++ src/main/java/com/hans/consumer/OrderConsumer.java
@@ -2,12 +2,15 @@ package com.hans.consumer;
 
 
 import com.hans.event.Order;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnBooleanProperty(name = "enable.record.consumer")
 public class OrderConsumer {
 
   private static final Logger log = LoggerFactory.getLogger(OrderConsumer.class);
@@ -18,6 +21,12 @@ public class OrderConsumer {
 
     log.info("Consumed order is : {}", order.orderId());
 
+  }
+
+  @KafkaListener(topics = "order-events-topic", groupId = "order-consumer-record-group")
+  public void consumeRecord(ConsumerRecord<String, Object> record) {
+    log.info("Consumer headers: {}", record.headers());
+    log.info("Consumed order deserialized is : {}", ((Order)record.value()).orderId());
   }
 
 }
